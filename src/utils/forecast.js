4efdf44aff119654bc72189758d37904949b9029
parthrk <parthrk@gmail.com>
2021-04-23 18:02:15 +0530

    Add more weather information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,12 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=c85c5150477b32e7d4c0019be5518af4&query=' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
-        console.log(body.current.weather_descriptions)
         if (error) {
             callback('Unable to connect to weather service', undefined)
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]} Weather, It is currently ${body.current.temperature} degrees out. There is ${body.current.precip}% chance of rain. `)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. There is ${body.current.precip}% chance of rain. The humidity is ${body.current.humidity}%`)
         }
     })
 }
